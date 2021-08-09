41a74476c7a98932828ea4c6b9067cd9ef096093
MarcoFavorito <marco.favorito@gmail.com>
2021-08-09 14:41:51 +0200

    test: add more tests for NNF

--- libs/logic/tests/unit/test_nnf.cpp
+++ libs/logic/tests/unit/test_nnf.cpp
@@ -76,6 +76,104 @@ TEST_CASE("Test NNF of doubly negated atomic") {
   REQUIRE(a == actual_formula);
 }
 
+TEST_CASE("Test NNF of next") {
+  // ~X[!]a = X(!a | end)
+  auto context = Context();
+  auto a = context.make_atom("a");
+  auto not_a = context.make_prop_not(a);
+  auto end = context.make_end();
+  auto not_a_or_end = context.make_or(vec_ptr{not_a, end});
+  auto next_a = context.make_next(a);
+  auto not_next_a = context.make_not(next_a);
+  auto weak_next_not_a = context.make_weak_next(not_a_or_end);
+  auto actual = to_nnf(*not_next_a);
+  const auto& expected = weak_next_not_a;
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("Test NNF of weak next") {
+  // ~X a = X[!](!a | end)
+  auto context = Context();
+  auto a = context.make_atom("a");
+  auto not_a = context.make_prop_not(a);
+  auto end = context.make_end();
+  auto weak_next_a = context.make_weak_next(a);
+  auto not_a_or_end = context.make_or(vec_ptr{not_a, end});
+  auto next_not_a = context.make_next(not_a_or_end);
+  auto not_weak_next_a = context.make_not(weak_next_a);
+  auto actual = to_nnf(*not_weak_next_a);
+  const auto& expected = next_not_a;
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("Test NNF of until") {
+  // ~(a U b) = ((!a | end) R (!b | end))
+  auto context = Context();
+  auto a = context.make_atom("a");
+  auto b = context.make_atom("b");
+  auto not_a = context.make_prop_not(a);
+  auto not_b = context.make_prop_not(b);
+  auto end = context.make_end();
+  auto not_a_or_end = context.make_or(vec_ptr{not_a, end});
+  auto not_b_or_end = context.make_or(vec_ptr{not_b, end});
+  auto a_until_b = context.make_until(vec_ptr{a, b});
+  auto not_a_until_b = context.make_not(a_until_b);
+  auto not_a_release_not_b =
+      context.make_release(vec_ptr{not_a_or_end, not_b_or_end});
+  auto actual = to_nnf(*not_a_until_b);
+  const auto& expected = not_a_release_not_b;
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("Test NNF of release") {
+  // ~(a R b) = ((!a | end) U (!b | end))
+  auto context = Context();
+  auto a = context.make_atom("a");
+  auto b = context.make_atom("b");
+  auto not_a = context.make_prop_not(a);
+  auto not_b = context.make_prop_not(b);
+  auto end = context.make_end();
+  auto not_a_or_end = context.make_or(vec_ptr{not_a, end});
+  auto not_b_or_end = context.make_or(vec_ptr{not_b, end});
+  auto a_release_b = context.make_release(vec_ptr{a, b});
+  auto not_a_release_b = context.make_not(a_release_b);
+  auto not_a_until_not_b =
+      context.make_until(vec_ptr{not_a_or_end, not_b_or_end});
+  auto actual = to_nnf(*not_a_release_b);
+  const auto& expected = not_a_until_not_b;
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("Test NNF of eventually") {
+  // ~Fa = G(!a | end)
+  auto context = Context();
+  auto a = context.make_atom("a");
+  auto not_a = context.make_prop_not(a);
+  auto end = context.make_end();
+  auto not_a_or_end = context.make_or(vec_ptr{not_a, end});
+  auto eventually_a = context.make_eventually(a);
+  auto not_eventually_a = context.make_not(eventually_a);
+  auto always_not_a = context.make_always(not_a_or_end);
+  auto actual = to_nnf(*not_eventually_a);
+  const auto& expected = always_not_a;
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("Test NNF of always") {
+  // ~Ga = F(!a | end)
+  auto context = Context();
+  auto a = context.make_atom("a");
+  auto not_a = context.make_prop_not(a);
+  auto end = context.make_end();
+  auto not_a_or_end = context.make_or(vec_ptr{not_a, end});
+  auto always_a = context.make_always(a);
+  auto not_always_a = context.make_not(always_a);
+  auto eventually_not_a = context.make_eventually(not_a_or_end);
+  auto actual = to_nnf(*not_always_a);
+  const auto& expected = eventually_not_a;
+  REQUIRE(actual == expected);
+}
+
 } // namespace Test
 } // namespace logic
 } // namespace cynthia
